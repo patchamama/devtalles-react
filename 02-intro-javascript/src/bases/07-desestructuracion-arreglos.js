0ace2f9d57eb523489adf440650392daf9715a49
A. Urquiola <patchamama.go@gmail.com>
2023-10-31 22:07:53 +0100

    07-desestructuracion-arreglos

---
+++ 02-intro-javascript/src/bases/07-desestructuracion-arreglos.js
@@ -0,0 +1,27 @@
+const personajes = ['Goku', 'Vegeta', 'Trunks']
+
+const [, , var3] = personajes
+console.log(var3)
+
+const retornaArreglo = () => {
+  return [('ABC', 123)]
+}
+
+const [letras, numeros] = retornaArreglo()
+console.log(letras, numeros)
+
+const useState = (valor) => {
+  return [
+    valor,
+    () => {
+      console.log('Hola mundo')
+    },
+  ]
+}
+
+const arr = useState('Goku')
+arr[1]()
+
+const [nombre, setNombre] = useState('Goku')
+console.log(nombre)
+setNombre() // parecido a arr[1]()
