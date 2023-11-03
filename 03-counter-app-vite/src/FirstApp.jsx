426e5104ffb548715cc10f42f87d3e0fd9af4376
A. Urquiola <patchamama.go@gmail.com>
2023-11-03 13:33:56 +0100

    Impresión de variables en el HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,13 +1,23 @@
-// import { Fragment } from 'react'
+// Sí no hay ninguna dependencia del component con constantes, variables o funciones
+// mejor definirla fuera del componente (functional component).
 
+const newMessage = {
+  message: 'Hola Mundo',
+  title: 'Fernando',
+}
+
+const getResult = (a, b) => {
+  return a + b
+}
+
+// ----------
 export const FirstApp = () => {
   return (
     <>
-      {/* <Fragment> */}
-      <h1>Fernando</h1>
-      <p>Spy un subtitulo</p>
-      <p>Spy un subtitulo</p>
-      {/* </Fragment> */}
+      {/* <code>{newMessage}</code>  esto devuelve un error pues newMessage es un objeto */}
+      <code>{JSON.stringify(newMessage)}</code>
+      <p>Soy un subtitulo</p>
+      <p>{getResult(1, 2)}</p>
     </>
   )
 }
