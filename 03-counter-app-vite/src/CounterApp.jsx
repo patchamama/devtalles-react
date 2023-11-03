7a6368db9edce975d2c3c0d677c066c8a83c630f
A. Urquiola <patchamama.go@gmail.com>
2023-11-03 15:05:11 +0100

    useState - Hook

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,26 +1,26 @@
 //https://cursos.devtalles.com/courses/take/react-cero-experto/lessons/36057304-tarea-componente-counterapp
 //https://cursos.devtalles.com/courses/take/react-cero-experto/lessons/36057309-evento-click-eventos-en-general
 //https://es.legacy.reactjs.org/docs/events.html
+//https://cursos.devtalles.com/courses/take/react-cero-experto/lessons/36057314-usestate-hook
 
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
-// Normalmente se recomienda usar el handleEvent aquí pero como se usará
-// con el Hook internamente en el próximo paso
-// const handleEvent = (event) => {
-//   console.log(event)
-// }
-
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value)
+
   const handleEvent = (event) => {
-    console.log(event)
+    //   setCounter(counter + 1)
+    setCounter((c) => c + 1)
   }
   return (
     <>
       <h1>Counter App</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
 
-      <button onClick={(event) => handleEvent(event)}>+1</button>
       <button onClick={handleEvent}>+1</button>
+      <button onClick={(event) => handleEvent(event)}>+1</button>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
     </>
   )
 }
