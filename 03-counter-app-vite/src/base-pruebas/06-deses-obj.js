e49496da89e66a5428748b78e37c638b8edb3571
A. Urquiola <patchamama.go@gmail.com>
2023-11-06 16:12:57 +0100

    Pruebas en el archivo 07-deses.arr.js

--- 03-counter-app-vite/src/base-pruebas/06-deses-obj.js
+++ 03-counter-app-vite/src/base-pruebas/06-deses-obj.js
@@ -1,37 +1,12 @@
-
-// Desestructuración
-// Asignación Desestructurante
-const persona = {
-    nombre: 'Tony',
-    edad: 45,
-    clave: 'Ironman'
-};
-
-// const { edad, clave, nombre, } = persona;
- 
-// console.log( nombre );
-// console.log( edad );
-// console.log( clave );
-
-const usContext = ({ clave, nombre, edad, rango = 'Capitán' }) => {
-
-
-    // console.log( nombre, edad, rango );
-    
-    return {
-        nombreClave: clave,
-        anios: edad,
-        latlng: {
-            lat: 14.1232,
-            lng: -12.3232
-        }
-    }
-
+export const usContext = ({ clave, nombre, edad, rango = 'Capitán' }) => {
+  return {
+    nombreClave: clave,
+    anios: edad,
+    latlng: {
+      lat: 14.1232,
+      lng: -12.3232,
+    },
+  }
 }
 
-const { nombreClave, anios, latlng: { lat, lng } } = usContext( persona );
-
-console.log(nombreClave, anios);
-console.log( lat, lng );
-
-
+// const { nombreClave, anios, latlng: { lat, lng } } = usContext( persona );
