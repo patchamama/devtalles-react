4f51e4126b42a5579a65140ddb104b491580827b
A. Urquiola <patchamama.go@gmail.com>
2023-11-07 13:31:43 +0100

    Probar FirstApp (toMatchSnapshot)

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -9,7 +9,7 @@ export const FirstApp = ({ title, subTitle, name }) => {
 
   return (
     <>
-      <h1>{title}</h1>
+      <h1> {title} </h1>
       <p>{subTitle}</p>
       <p>{name}</p>
     </>
--- 03-counter-app-vite/src/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/src/tests/FirstApp.test.jsx
@@ -4,6 +4,18 @@ import { FirstApp } from '../FirstApp'
 describe('Pruebas en el archivo FirstApp.test.jsx', () => {
   test('debe de hacer match con el snapshot', () => {
     const title = 'Hola, Soy Goku'
-    render(<FirstApp title={title} />)
+    const { container } = render(<FirstApp title={title} />)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  test('debe de mostrar el titulo en un h1', () => {
+    const title = 'Hola, Soy Goku'
+    const { container, getByText } = render(<FirstApp title={title} />)
+
+    expect(getByText(title)).toBeTruthy()
+
+    const h1 = container.querySelector('h1')
+    expect(h1.innerHTML).toContain(title)
   })
 })
