53b3233231dccec285a475cf1ac116a02096c337
A. Urquiola <patchamama.go@gmail.com>
2023-11-11 11:39:08 +0100

    usseEffect - SimpleForm

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: 'strider',
+    email: 'fernando@google.com',
+  })
+
+  const { username, email } = formState
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target
+    setFormState({ ...formState, [name]: value })
+  }
+
+  useEffect(() => {
+    console.log('useEffect called!')
+  }, [])
+
+  return (
+    <>
+      <h1>Formulario Simple</h1>
+      <hr />
+      <input
+        type='text'
+        className='form-control'
+        placeholder='UserName'
+        name='username'
+        value={username}
+        onChange={onInputChange}
+      />
+      <input
+        type='email'
+        className='form-control mt-2'
+        placeholder='fernando@goole.com'
+        name='email'
+        value={email}
+        onChange={onInputChange}
+      />
+    </>
+  )
+}
