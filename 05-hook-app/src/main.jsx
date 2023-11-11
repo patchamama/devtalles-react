c71a5e556aead9375d1f5aeb1a61c9817a86bc7c
A. Urquiola <patchamama.go@gmail.com>
2023-11-11 22:54:56 +0100

    useRef - Primer uso

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -6,10 +6,11 @@ import './index.css'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </React.StrictMode>
 )
