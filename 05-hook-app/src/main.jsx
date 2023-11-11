fbb02015f14b74f8bdb915b32dc350f7b4f749ab
A. Urquiola <patchamama.go@gmail.com>
2023-11-11 11:06:37 +0100

    useState

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HooksApp } from './HooksApp'
+// import { HooksApp } from './HooksApp'
+import { CounterApp } from './01-useState/CounterApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    <CounterApp />
   </React.StrictMode>
 )
