31414f3bbf937fc6ea46e4c8082783b28807d84d
A. Urquiola <patchamama.go@gmail.com>
2023-11-12 01:04:36 +0100

    useMemo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -9,10 +9,11 @@ import './index.css'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+// import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   </React.StrictMode>
 )
