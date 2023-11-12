37ac05231af0aa84b8a353607d1ffb6b3d3c767b
A. Urquiola <patchamama.go@gmail.com>
2023-11-12 11:29:52 +0100

    Continuación del Proyecto - HookApp

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -14,8 +14,10 @@ import './index.css'
 // import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Padre />
-  // </React.StrictMode>
-)
+import './08-useReducer/intro-reducer'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//   <Padre />
+//   // </React.StrictMode>
+// )
