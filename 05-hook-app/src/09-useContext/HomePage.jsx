a6c77ab5548f75d1bbd913d758af43af42e57d97
A. Urquiola <patchamama.go@gmail.com>
2023-11-14 17:13:21 +0100

    Pruebas con useContext

--- 05-hook-app/src/09-useContext/HomePage.jsx
+++ 05-hook-app/src/09-useContext/HomePage.jsx
@@ -11,7 +11,7 @@ export const HomePage = () => {
         HomePage <small>{user?.name}</small>
       </h1>
       <hr />
-      <pre>{JSON.stringify(user, null, 3)}</pre>
+      <pre aria-label='pre'>{JSON.stringify(user, null, 3)}</pre>
     </>
   )
 }
