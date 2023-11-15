6cd2e604ac8f29a65abb4aec5d0b1b3a2c0759aa
A. Urquiola <patchamama.go@gmail.com>
2023-11-15 09:04:22 +0100

    Creando un segundo Router

---
+++ 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Routes, Route } from 'react-router-dom'
+
+import { Navbar } from '../../ui'
+import { MarvelPage, DcPage, SearchPage, HeroPage } from '../pages'
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className='container'>
+        <Routes>
+          <Route path='marvel' element={<MarvelPage />} exact />
+          <Route path='dc' element={<DcPage />} exact />
+
+          <Route path='search' element={<SearchPage />} exact />
+          <Route path='hero' element={<HeroPage />} exact />
+
+          <Route path='/' element={<Navigate to='/marvel' />} exact />
+        </Routes>
+      </div>
+    </>
+  )
+}
--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,21 +1,16 @@
-import { Navigate, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import { LoginPage } from '../auth'
-import { MarvelPage, DcPage } from '../heroes'
 import { Navbar } from '../ui'
+import { HeroesRoutes } from '../heroes'
 
 export const AppRouter = () => {
   return (
     <>
-      <Navbar />
-
       <Routes>
-        <Route path='marvel' element={<MarvelPage />} exact />
-        <Route path='dc' element={<DcPage />} exact />
-
         <Route path='login' element={<LoginPage />} exact />
 
-        <Route path='/' element={<Navigate to='/marvel' />} exact />
+        <Route path='/*' element={<HeroesRoutes />} exact />
       </Routes>
     </>
   )
