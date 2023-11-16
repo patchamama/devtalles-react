94365b42fd946365b7a64cb368ef167501957f1d
A. Urquiola <patchamama.go@gmail.com>
2023-11-16 23:21:17 +0100

    Rutas privadas

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { LoginPage } from '../auth'
 import { Navbar } from '../ui'
 import { HeroesRoutes } from '../heroes'
+import { PrivateRoute } from './PrivateRoute'
 
 export const AppRouter = () => {
   return (
@@ -10,7 +11,15 @@ export const AppRouter = () => {
       <Routes>
         <Route path='login' element={<LoginPage />} exact />
 
-        <Route path='/*' element={<HeroesRoutes />} exact />
+        <Route
+          path='/*'
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+        {/* <Route path='/*' element={<HeroesRoutes />} exact /> */}
       </Routes>
     </>
   )
