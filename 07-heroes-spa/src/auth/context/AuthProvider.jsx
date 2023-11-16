030674302f0a0f94ee41a73475caa44684c035e5
A. Urquiola <patchamama.go@gmail.com>
2023-11-16 23:02:30 +0100

    Mantener el usuario activo

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -3,14 +3,26 @@ import { AuthContext } from './AuthContext'
 import { authReducer } from './authReducer'
 import { types } from '../types/types'
 
-const initialState = {
-  logged: false,
+// const initialState = {
+//   logged: false,
+// }
+
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'))
+  return {
+    logged: !!user,
+    user: user,
+  }
 }
 
 export const AuthProvider = ({ children }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState)
+  const [authState, dispatch] = useReducer(authReducer, {}, init)
 
   const login = (name = '') => {
+    const user = {
+      id: 'ABC',
+      name,
+    }
     const action = {
       type: types.login,
       payload: {
@@ -18,6 +30,9 @@ export const AuthProvider = ({ children }) => {
         id: 'ABC',
       },
     }
+
+    localStorage.setItem('user', JSON.stringify(user))
+
     dispatch(action)
   }
 
