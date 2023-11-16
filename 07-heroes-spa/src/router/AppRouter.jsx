d9e7793f01c631d520ce938b50e02a9e12c78934
A. Urquiola <patchamama.go@gmail.com>
2023-11-16 23:30:50 +0100

    Rutas públicas

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -4,12 +4,21 @@ import { LoginPage } from '../auth'
 import { Navbar } from '../ui'
 import { HeroesRoutes } from '../heroes'
 import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path='login' element={<LoginPage />} exact />
+        <Route
+          path='/login'
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        {/* <Route path='login' element={<LoginPage />} exact /> */}
 
         <Route
           path='/*'
