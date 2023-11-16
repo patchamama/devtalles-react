435a396d00e301a4564a4c5d5d2ab142d847c54f
A. Urquiola <patchamama.go@gmail.com>
2023-11-17 00:54:01 +0100

    Pruebas en el PublicRoute

--- 07-heroes-spa/src/auth/index.js
+++ 07-heroes-spa/src/auth/index.js
@@ -1,3 +1,3 @@
-export * from './pages'
 export * from './context'
-export * from './types'
+export * from './pages'
+export * from './types/types'
--- 07-heroes-spa/src/router/PublicRoute.jsx
+++ 07-heroes-spa/src/router/PublicRoute.jsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react'
-import { AuthContext } from '../auth'
 import { Navigate } from 'react-router-dom'
+
+import { AuthContext } from '../auth'
 
 export const PublicRoute = ({ children }) => {
   const { logged } = useContext(AuthContext)
 
-  return logged ? <Navigate to='/marvel' /> : children
+  return !logged ? children : <Navigate to='/marvel' />
 }
