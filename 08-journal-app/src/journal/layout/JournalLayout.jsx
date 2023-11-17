45348a196315e917497415a6c5bed08146efa2c6
A. Urquiola <patchamama.go@gmail.com>
2023-11-17 13:20:06 +0100

    JournalLayout y JournalPage

---
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,19 @@
+import { Box } from '@mui/system'
+
+const drawerWidth = 280
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      {/* <NavBar drawerWidth={drawerWidth} />
+
+      <SideBar drawerWidth={drawerWidth} /> */}
+
+      <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+        {/* <Toolbar /> */}
+
+        {children}
+      </Box>
+    </Box>
+  )
+}
--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,9 +1,16 @@
+import { JournalLayout } from '../layout/JournalLayout'
 import { Typography } from '@mui/material'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'> JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate
+        duis laboris. Esse esse consectetur ullamco excepteur ullamco amet.
+        Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud
+        incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco
+        ipsum do adipisicing irure.
+      </Typography>
+    </JournalLayout>
   )
 }
