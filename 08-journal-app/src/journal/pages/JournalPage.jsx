89766e7e1368055c95df1ee2c078d532c964ff89
A. Urquiola <patchamama.go@gmail.com>
2023-11-17 13:34:34 +0100

    NothingSelectedView - No hay nada seleccionado

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,16 +1,18 @@
 import { JournalLayout } from '../layout/JournalLayout'
 import { Typography } from '@mui/material'
+import { NothingSelectedView } from '../views/NothingSelectedView'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate
         duis laboris. Esse esse consectetur ullamco excepteur ullamco amet.
         Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud
         incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco
         ipsum do adipisicing irure.
-      </Typography>
+      </Typography> */}
+      <NothingSelectedView />
     </JournalLayout>
   )
 }
