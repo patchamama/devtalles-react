dc72011667821a6aafbfa2261cc59cb8cb302707
A. Urquiola <patchamama.go@gmail.com>
2023-11-17 16:57:49 +0100

    ConfigureStore y Slices

--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Provider } from 'react-redux'
+import App from './App'
 import './index.css'
+// import { PokemonApp } from './PokemonApp';
+
+import { store } from './store'
+// import { TodoApp } from './TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
